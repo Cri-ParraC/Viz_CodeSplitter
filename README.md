0b13eef809f32f7dc9b2618ca7bec450df94f4bf
Cristian Parra <cri.parrac@gmail.com>
2025-07-06 18:56:17 -0400

    Añadida y modificada documentación

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# Viz_CodeSplitter
+# Viz_CodeSplitter [MZ] (v1.0.0)
 
-Plugin para RPG Maker MZ que una vez activo, divide el en archivos pequeños.
+Plugin para RPG Maker MZ que divide el Core Script del proyecto actual en archivos con una clase cada uno.
+
+Hasta no tener un release, se debe construir mediante comandos.
+
+## Construcción
+
+Se debe clonar desde el directorio js/plugins:
+
+```sh
+git clone git@github.com:Cri-ParraC/Viz_CodeSplitter.git
+```
+
+Construir el plugin en js/plugins, compatible para NW.js:
+
+```sh
+npm run build
+```
--- Viz_CodeSplitter.js
+++ Viz_CodeSplitter.js
@@ -1,79 +1,103 @@
-try {
-  const fs = require('fs');
-  const path = require('path');
+//=============================================================================
+// Viz_CodeSplitter.js [MZ] (v1.0.1)
+//=============================================================================
 
-  const jsPath = path.join(__dirname, '..', '..');
-  const jsPathFileNames = fs.readdirSync(jsPath);
+/*:
+ * @target MZ
+ * @plugindesc [MZ] (v1.0.0)
+ * @author Vizcacha
+ * @url https://github.com/Cri-ParraC/Viz_CodeSplitter
+ * @help Viz_CodeSplitter.js [MZ] (v1.0.0)
+ * ----------------------------------------------------------------------------
+ *
+ */
 
-  if (jsPathFileNames.length === 0)
-    throw new Error('No files found in jsPath');
+(() => {
+  "use strict";
 
-  const classDivider = "//" + '='.repeat(77);
-  const sectionDivider = "//" + '-'.repeat(77);
+  console.info("Viz_CodeSplitter.js [MZ] (v1.0.1)");
 
-  const rmmzFileNames = jsPathFileNames.filter((fileName) => {
-    return fileName.startsWith('rmmz_') && fileName.endsWith('.js');
-  });
+  window.Imported ??= {};
+  Imported.Viz_CodeSplitter = 1.0;
 
-  const scriptsByFileName = {};
+  try {
+    const fs = require('fs');
+    const path = require('path');
 
-  rmmzFileNames.forEach((fileName) => {
-    const filePath = path.join(jsPath, fileName);
-    const fileData = fs.readFileSync(filePath, 'utf8');
-    const fileExtension = path.extname(fileName);
-    const fileBaseName = path.basename(fileName, fileExtension);
-    const directoryPath = path.join(jsPath, fileBaseName);
+    const jsPath = path.join(__dirname, '..', '..');
+    const jsPathFileNames = fs.readdirSync(jsPath);
 
-    fs.mkdirSync(directoryPath, { recursive: true });
+    if (jsPathFileNames.length === 0)
+      throw new Error('No files found in jsPath');
 
-    const headerMatches = fileData.match(new RegExp(`^(${classDivider}\\n.*?\\n${classDivider})`, 'm'));
+    const classDivider = "//" + '='.repeat(77);
+    const sectionDivider = "//" + '-'.repeat(77);
 
-    if (headerMatches == null)
-      throw new Error("Header not found in " + fileName);
-
-    const splitClassesNames = [];
-
-    if (fileName === 'rmmz_core.js') {
-      const jsExtensions = fileData.split(sectionDivider)[1].trimEnd() + '\n';
-      const content = headerMatches[1] + '\n' + sectionDivider + jsExtensions;
-      fs.writeFileSync(path.join(directoryPath, 'JsExtensions' + '.js'), content);
-      splitClassesNames.push('JsExtensions');
-    }
-
-    const splitSections = fileData.split(sectionDivider).filter((splitClass) => {
-      const firstLine = splitClass.trim().split('\n')[0].trim();
-      return firstLine.startsWith('// ') || firstLine.startsWith('/**');
+    const rmmzFileNames = jsPathFileNames.filter((fileName) => {
+      return fileName.startsWith('rmmz_') && fileName.endsWith('.js');
     });
 
-    const splitClasses = splitSections.map((splitClass) => sectionDivider + splitClass.trimEnd() + '\n');
+    const scriptsByFileName = {};
 
-    splitClasses.forEach((splitClass) => {
-      const constructorMatch = splitClass.match(/function\s+([A-Za-z0-9_]+)\s*\(/);
+    rmmzFileNames.forEach((fileName) => {
+      const filePath = path.join(jsPath, fileName);
+      const fileData = fs.readFileSync(filePath, 'utf8');
+      const fileExtension = path.extname(fileName);
+      const fileBaseName = path.basename(fileName, fileExtension);
+      const directoryPath = path.join(jsPath, fileBaseName);
 
-      if (constructorMatch != null) {
-        const content = headerMatches[1] + '\n\n' + splitClass;
-        fs.writeFileSync(path.join(directoryPath, constructorMatch[1] + '.js'), content);
-        splitClassesNames.push(constructorMatch[1]);
+      fs.mkdirSync(directoryPath, { recursive: true });
+
+      const headerMatches = fileData.match(new RegExp(`^(${classDivider}\\n.*?\\n${classDivider})`, 'm'));
+
+      if (headerMatches == null)
+        throw new Error("Header not found in " + fileName);
+
+      const splitClassesNames = [];
+
+      if (fileName === 'rmmz_core.js') {
+        const jsExtensions = fileData.split(sectionDivider)[1].trimEnd() + '\n';
+        const content = headerMatches[1] + '\n' + sectionDivider + jsExtensions;
+        fs.writeFileSync(path.join(directoryPath, 'JsExtensions' + '.js'), content);
+        splitClassesNames.push('JsExtensions');
       }
+
+      const splitSections = fileData.split(sectionDivider).filter((splitClass) => {
+        const firstLine = splitClass.trim().split('\n')[0].trim();
+        return firstLine.startsWith('// ') || firstLine.startsWith('/**');
+      });
+
+      const splitClasses = splitSections.map((splitClass) => sectionDivider + splitClass.trimEnd() + '\n');
+
+      splitClasses.forEach((splitClass) => {
+        const constructorMatch = splitClass.match(/function\s+([A-Za-z0-9_]+)\s*\(/);
+
+        if (constructorMatch != null) {
+          const content = headerMatches[1] + '\n\n' + splitClass;
+          fs.writeFileSync(path.join(directoryPath, constructorMatch[1] + '.js'), content);
+          splitClassesNames.push(constructorMatch[1]);
+        }
+      });
+
+      scriptsByFileName[fileBaseName] = splitClassesNames;
     });
 
-    scriptsByFileName[fileBaseName] = splitClassesNames;
-  });
+    const mainPath = path.join(jsPath, 'main.js');
+    let mainData = fs.readFileSync(mainPath, 'utf8');
 
-  const mainPath = path.join(jsPath, 'main.js');
-  let mainData = fs.readFileSync(mainPath, 'utf8');
+    for (const fileName in scriptsByFileName) {
+      const scriptUrl = `    "js/${fileName}.js",`;
 
-  for (const fileName in scriptsByFileName) {
-    const scriptUrl = `    "js/${fileName}.js",`;
-
-    if (mainData.includes(scriptUrl)) {
-      const scriptUrlsByFileName = scriptsByFileName[fileName].map((className) =>
-        `    "js/${fileName}/${className}.js"`);
-      mainData = mainData.replace(scriptUrl, scriptUrlsByFileName.join(',\n') + ',\n');
+      if (mainData.includes(scriptUrl)) {
+        const scriptUrlsByFileName = scriptsByFileName[fileName].map((className) =>
+          `    "js/${fileName}/${className}.js"`);
+        mainData = mainData.replace(scriptUrl, scriptUrlsByFileName.join(',\n') + ',\n');
+      }
     }
+    fs.writeFileSync(mainPath, mainData);
+  } catch (error) {
+    console.error("An error occurred while splitting the code");
+    console.error(error.stack ?? error.message);
   }
-  fs.writeFileSync(mainPath, mainData);
-} catch (error) {
-  console.error("An error occurred while splitting the code");
-  console.error(error.stack ?? error.message);
-}
+
+})();
